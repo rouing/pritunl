dfcdf28ad86db43e941e626db9960e2293a24f7d
William C Ardoin <hostmaster@rouing.me>
2018-12-06 20:40:18 +0000

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,16 +1,20 @@
 set -ex
 
-echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
-echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-
 apt-get update -q
 apt-get upgrade -y -q
 
-apt-get install -y locales iptables wget gnupg2
+apt-get install -y gnupg2 wget
+
+echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
+echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
+
+apt-get update -q
+
+apt-get install -y locales iptables 
 
 locale-gen en_US en_US.UTF-8
 dpkg-reconfigure locales
