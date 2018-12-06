16fc5401f00d553d9446382e65cb09a7abb95e29
William C Ardoin <hostmaster@rouing.me>
2018-12-06 22:08:17 +0000

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,7 +3,7 @@ set -ex
 apt-get update -q
 apt-get upgrade -y -q
 
-apt-get install -y gnupg2 wget
+apt-get install -y gnupg2 wget curl
 
 echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
 echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
@@ -13,19 +13,18 @@ wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
 
 apt-get update -q
 
-apt-get install -y locales iptables 
+apt-get install -y locales iptables
 
 locale-gen en_US en_US.UTF-8
-dpkg-reconfigure locales
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 PRITUNL_VERSION=$(curl --silent "https://api.github.com/repos/pritunl/pritunl/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
-dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb || apt-get -f -y install
-rm pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
+wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb
+dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb || apt-get -f -y install
+rm pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb
 
-apt-get --purge autoremove -y wget
+apt-get --purge autoremove -y wget curl
 apt-get -y -q autoclean
 apt-get -y -q autoremove
 rm -rf /tmp/*
